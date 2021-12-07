5d649425f2836c9df780c5191565211cb0583729
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-12-07 11:38:16 +0100

    (typo)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,7 +22,7 @@
 
             <!-- FullName -->
             <div class="mt-4">
-                <x-label for="fullName" :value="__('Full name')"/>
+                <x-label for="fullName" :value="__('Full Name')"/>
 
                 <x-input id="fullName" class="block mt-1 w-full" type="text" name="fullName" :value="old('fullName')" required
                          autofocus/>
