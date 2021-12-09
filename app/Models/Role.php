4ea426583d7d44393eaf57b1d19660188606e64f
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-12-09 10:08:27 +0100

    add function to get Role id form slug

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,4 +9,11 @@ class Role extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    static function idFromSlug($slug){
+        self::whereHas(
+            'id',
+            fn($getId) => $getId->where('slug',"=",$slug)
+        );
+    }
 }
