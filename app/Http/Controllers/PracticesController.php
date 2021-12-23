9e9fc2aa36449ba7bda513a6566054b7cb535c51
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-12-23 16:36:51 +0100

    change PracticeController

--- app/Http/Controllers/PracticesController.php
+++ app/Http/Controllers/PracticesController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Practice;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+
 
 class PracticesController extends Controller
 {
-    public function index():View
+    public function show($id):View|RedirectResponse
     {
-        return view("practice");
+        $practice = Practice::find($id);
+        if(null == $practice || "PUB" != $practice->publicationState->slug ){
+            return redirect()->route('home');
+        }else{
+            return view("practice")->with(['practice' => $practice]);
+        }
+
     }
 }
