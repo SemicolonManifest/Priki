0b6d27c07bb183b5d81ef8bd0879700b6bf59d87
TGR <78166079+TGACPNV@users.noreply.github.com>
2022-01-27 14:54:47 +0100

    add edit form

---
+++ resources/js/practice.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btnEditTitle").addEventListener("click", function() {
+        document.getElementById("editTitleFormGroup").hidden = false;
+    })
+});
